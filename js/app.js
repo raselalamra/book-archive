9894282538103d0af49ce9249e79304724beaa18
raselalamjubayed <raselalam838@gmil.com>
2021-09-02 14:34:44 +0600

    add books

--- js/app.js
+++ js/app.js
@@ -8,25 +8,30 @@ const searchBook = () =>{
      const url = `https://openlibrary.org/search.json?q=${searchText}`
      fetch(url)
      .then(res => res.json())
-     .then (data => displaySearchResult(data.docs))
+     .then (data => displaySearchResult(data))
 }
 
-const displaySearchResult = docs => {
+const displaySearchResult = data => {
     const searchResult = document.getElementById('search-result');
-    docs.forEach(book => {
+    const dataArray = data.docs
+    dataArray.forEach(book => {
         console.log(book)
         const div = document.createElement('div')
         div.classList.add('col');
         div.innerHTML = `
         <div class="card h-100">
-            <img src="..." class="card-img-top" alt="...">
+            <img src="https://covers.openlibrary.org/b/id/${book.cover_i}-M.jpg" class="card-img-top" alt="No Found">
             <div class="card-body">
                  <h5 class="card-title">${book.title}</h5>
-                 <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                 <h6>Author Name: ${book.author_name}</h6>
+                 <p>publisher: ${book.publisher}</p>
+                 <p>First Publish: ${book.first_publish_year}</p>
             </div>
        </div>
         `;
         searchResult.appendChild(div);
+        const totalResult = document.getElementById('totalResult')
+        totalResult.innerHTML = `<p>Resutl Found: ${data.numFound}</p>`
     });
    
 }
